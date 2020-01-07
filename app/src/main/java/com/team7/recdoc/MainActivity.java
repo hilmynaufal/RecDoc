0f74ad9d79bc9b7a2d2bd8912793d093f5cc7baa
hilmynaufal <hilmyblaze@gmail.com>
2020-01-07 20:14:11 +0700

    benerin fragment

--- app/src/main/java/com/team7/recdoc/MainActivity.java
+++ app/src/main/java/com/team7/recdoc/MainActivity.java
@@ -29,9 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
 
 
-    public static final String BASE_API_URL = "https://web-halodoc-api.prod.halodoc.com/";
-    private ListView listView;
-    private List<Content> content;
+
 
 
     @Override
@@ -46,47 +44,6 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         BottomNavigationView bottomNavigationView = findViewById(R.id.bn_main);
         // beri listener pada saat item/menu bottomnavigation terpilih
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
-
-
-
-
-
-        listView = findViewById(R.id.list_test);
-        final ProgressDialog loading = ProgressDialog.show(this, "Fetching data...", "please wait", false, false);
-
-        HttpLoggingInterceptor logging = new HttpLoggingInterceptor();
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY);
-        OkHttpClient.Builder httpclient = new OkHttpClient.Builder();
-        httpclient.addInterceptor(logging);
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl(MainActivity.BASE_API_URL)
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(httpclient.build())
-                .build();
-
-        APIService apiService = retrofit.create(APIService.class);
-        Call<Result> result = apiService.getResultInfo();
-        result.enqueue(new Callback<Result>() {
-            @Override
-            public void onResponse(Call<Result> call, Response<Result> response) {
-                try {
-                    loading.dismiss();
-                    content = response.body().getContent();
-
-                    showList();
-
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-
-            @Override
-            public void onFailure(Call<Result> call, Throwable t) {
-                t.printStackTrace();
-            }
-        });
 
     }
 
@@ -122,16 +79,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         return loadFragment(fragment);
     }
 
-    private void showList() {
-        String[] items = new String[content.size()];
-        for (int i = 0; i < content.size(); i++) {
-            items[i] = URLDecoder.decode(content.get(i).getTitle());
-        }
 
-        ArrayAdapter adapter = new ArrayAdapter<>(this, R.layout.activity_list, items);
-
-        listView.setAdapter(adapter);
-    }
 
 
 }
