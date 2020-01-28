cd873ca7910c9394667b1f240860e9eda2098f1d
Ilham377 <ilham377@gmail.com>
2020-01-28 23:08:08 +0700

    ganti nama

--- app/src/main/java/com/team7/recdoc/view/FoodFragment.java
+++ app/src/main/java/com/team7/recdoc/view/FoodFragment.java
@@ -46,7 +46,7 @@ public class FoodFragment extends Fragment implements View.OnClickListener {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        final View view = inflater.inflate(R.layout.fragment_lifestyle, container, false);
+        final View view = inflater.inflate(R.layout.fragment_food, container, false);
 
         textView = view.findViewById(R.id.edtSearchFood);
         FoodLayout = view.findViewById(R.id.llCalories);
